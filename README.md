88bd6f739904d12ea41efd76c8e348ced11f91c4
Mark Bolhuis <mark@bolhuis.dev>
2021-08-28 11:01:55 +0100

    Add more commands and examples

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This is a small python script to control Eizo monitors using the the [linux driver](https://github.com.com/openeizo/openeizo).
 
+----
 ## Example
 To read the brightness value:
 ```
@@ -11,4 +12,25 @@ $ python main.py brightness
 To set the brightness value:
 ```
 $ python main.py brightness -s 170
+```
+----
+To switch inputs:
+
+HDMI
+```
+$ python main.py input -s 1024
+```
+
+DVI
+```c
+$ python main.py input -s 512
+```
+
+PicbyPic DP1 & DP2
+```
+$ python main.py pic-by-pic -s 1
+$ python main.py screen -s 0
+$ python main.py input -s 768
+$ python main.py screen -s 1
+$ python main.py input -s 769
 ```
--- main.py
+++ main.py
@@ -7,8 +7,13 @@ settings = {
     'temp': 1,
     'saturation': 18,
     'hue': 19,
+    'pic-by-pic': 32,
     'lock-osd': 33,
     'disable-osd': 34,
+    'input': 35,
+    'screen': 50,
+    'osd-rotation': 55,
+    'debug': 59,
 
     # Consumer
     'volume': 81,
@@ -31,11 +36,12 @@ dev = hid.Device(0x056d, 0x40ff)
 setting = settings.get(args.setting)
 
 if args.set is not None:
-    data = setting.to_bytes(1, 'little', signed=False) + args.set.to_bytes(2, 'little', signed=False)
+    data = setting.to_bytes(1, 'little', signed=False) + args.set.to_bytes(32, 'little', signed=False)
     res = dev.send_feature_report(data)
+    print(res, 'bytes written')
 
 else:
-    data = dev.get_feature_report(setting, 3)
+    data = dev.get_feature_report(setting, 33)
     num = int.from_bytes(data[0:1], 'little', signed=False)
     value = int.from_bytes(data[1:3], 'little', signed=False)
 
